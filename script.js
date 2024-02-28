e6a7092e013b17b59538d78043e81692ba33e06d
dicle <dicledilek888@gmail.com>
2024-02-28 16:23:34 +0300

    fixed index.html page

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function addBook(name, author, pages, read) {
     pages: pages,
     read: read,
   });
+  displayBook();
+}
+
+function displayBook() {
+  bookList.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const bookItem = document.createElement("li");
+    bookItem.textContent = `Book name:${book.name}, Author:${book.author}, Pages:${book.pages}, Read:${book.read}`;
+    bookList.appendChild(bookItem);
+  });
 }
 
 addBtn.addEventListener("click", addBook);
