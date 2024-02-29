6b79672f8683e71a380e1928c2ec11fb9f9e4cfc
dicle <dicledilek888@gmail.com>
2024-02-29 07:19:55 +0300

    data can be added

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-const addBtn = document.querySelector(".newBook");
-const container = document.querySelector(".container");
+let data = [];
 
-let arry = [];
-function addBook(name, author, pages, read) {
-  arry.push({
-    name: name,
-    author: author,
-    pages: pages,
-    read: read,
-  });
+class Library {
+  constructor(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  addBook() {
+    data.push({
+      name: this.name,
+      author: this.author,
+      pages: this.pages,
+      read: this.read,
+    });
+    this.displayBook();
+  }
+
+  displayBook() {
+    container.innerHTML = "";
+    data.forEach((book) => {
+      if (this.name && this.author && this.pages) {
+        container.innerHTML += `Book name:${book.name}, Author:${book.author}, Pages:${book.pages}, Read:${book.read}`;
+      }
+    });
+  }
 }
 
-addBtn.addEventListener("click", addBook);
+const addBtn = document.querySelector(".newBook");
+const container = document.querySelector(".container");
+const nameInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readInput = document.getElementById("read");
+
+addBtn.addEventListener("click", function () {
+  const book = new Library(
+    nameInput.value,
+    authorInput.value,
+    pagesInput.value,
+    readInput.value
+  );
+  book.addBook();
+});
