309ecf00a87ae614d122fccb6ee0e6d94e56ec5a
dicle <dicledilek888@gmail.com>
2024-03-01 11:29:01 +0300

    added remove and toggle button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let data = [];
+const myLibrary = getData() || [];
 
 class Library {
   constructor(name, author, pages, read) {
@@ -6,31 +6,6 @@ class Library {
     this.author = author;
     this.pages = pages;
     this.read = read;
-  }
-  addBook() {
-    data.push({
-      name: this.name,
-      author: this.author,
-      pages: this.pages,
-      read: this.read,
-    });
-    this.displayBook();
-  }
-
-  displayBook() {
-    container.innerHTML = "";
-    data.forEach((book) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
-      if (this.name && this.author && this.pages) {
-        card.innerHTML = `
-        <p>Book name:${book.name}</p>
-        <p>Author:${book.author}</p>
-        <p>Pages:${book.pages}</p>
-        <p>isRead:${book.read ? "Yes" : "No"} </p>`;
-      }
-      container.appendChild(card);
-    });
   }
 }
 
@@ -41,6 +16,39 @@ const authorInput = document.getElementById("author");
 const pagesInput = document.getElementById("pages");
 const readInput = document.getElementById("read");
 
+function displayBook() {
+  container.innerHTML = "";
+  let data = getData();
+  data.forEach((book, index) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <h2>${book.name}</h2>
+    <p>Author:${book.author}</p>
+    <p>Pages:${book.pages}</p>
+    <p>Read:${book.read}</p>
+    <button data-index=${index}class="remove-book">Remove</button>
+   <button data-index=${index} class="toggle-btn">${
+      book.read === "yes"
+        ? "Marked Unread"
+        : book.read === "no"
+        ? "Marked Read"
+        : null
+    }</button>
+    `;
+  });
+}
+
+function addBook() {
+  data.push({
+    name: this.name,
+    author: this.author,
+    pages: this.pages,
+    read: this.read,
+  });
+  this.displayBook();
+}
+
 addBtn.addEventListener("click", function () {
   const book = new Library(
     nameInput.value,
@@ -49,7 +57,6 @@ addBtn.addEventListener("click", function () {
     readInput.value
   );
   book.addBook();
-
   nameInput.value = "";
   authorInput.value = "";
   pagesInput.value = "";
